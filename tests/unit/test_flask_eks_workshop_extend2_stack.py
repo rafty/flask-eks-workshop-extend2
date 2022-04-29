f3e241e6bff1cd9578cf709b04394ffe2f45962c
takashi <yagita.takashi@gmail.com>
2022-04-29 15:23:18 +0900

    first commit

--- tests/unit/test_flask_eks_workshop_extend2_stack.py
+++ tests/unit/test_flask_eks_workshop_extend2_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from flask_eks_workshop_extend2.flask_eks_workshop_extend2_stack import FlaskEksWorkshopExtend2Stack
+from _stacks.flask_eks_workshop_extend2_stack import FlaskEksWorkshopExtend2Stack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in flask_eks_workshop_extend2/flask_eks_workshop_extend2_stack.py
